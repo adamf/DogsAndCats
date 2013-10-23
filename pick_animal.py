6ddbe58cc6633c2a0672ad841f174485527fe136
Adam Fletcher <adamf@csh.rit.edu>
2013-10-23 19:34:34 -0400

    Show rectangle as the user is drawing it

--- pick_animal.py
+++ pick_animal.py
@@ -1,13 +1,98 @@
+import argparse
 import cv2, cv
 
+parser = argparse.ArgumentParser(
+    description='Image manipulator for computer vision positive sample creation.')
 
-image_path = "/Users/adamfletcher/Downloads/train/bw/"
-image_extension = ".jpg"
-image_prefix = "dog."
-crop_target_directory = "cropped_images/"
+parser.add_argument('--image_path', dest='image_path', default='.',
+                    help='Directory containing the images to process (default: .).')
+
+parser.add_argument('--image_prefix', dest='image_prefix', required=True,
+                    help='Prefix for images to process, eg for a set of images '
+                         'dog1.jpg through dog1000.jpg, --image_prefix should be "dog"')
+
+parser.add_argument('--image_extension', dest='image_extension', default='.jpg',
+                    help='Filename extension for images to process (default: .jpg).')
+
+parser.add_argument('--target_path', dest='target_path', default='.',
+                    help='Directory in which to store the processed images and metadata (default: .).')
+
+parser.add_argument('--metadata_filename', dest='metadata_filename', required=True,
+                    help='Filename to use for the processed image metadata.')
+
+parser.add_argument('--overwrite_metadata', dest='overwrite_metadata', default=False,
+                    help='If set, overwrite any existing metadata.')
+
+# We'll try to normalize the captured images' aspect ratio to a fixed ratio; later we can scale them all
+# to the same size. The normalization will only increase the size of the capture, never shrink it. By default
+# we seek to capture a square. For human faces, which are taller than they are long, a square is probably not
+# the best ratio. 
+parser.add_argument('--aspect_ratio_width', dest='aspect_ratio_width', default=1,
+                    help='Default width aspect ratio value for image size normalization (default:1)')
+
+parser.add_argument('--aspect_ratio_height', dest='aspect_ratio_height', default=1,
+                    help='Default height aspect ratio value for image size normalization (default:1)')
+
+args = parser.parse_args()
+
+PICK_CV2_WINDOW_HANDLE = "Image"
+
+class TrainingImage():
+    def __init__(self, source_image, filename):
+        self._source_image = source_image
+        self._path = filename
+        self._upper_point = {} 
+        self._lower_point = {} 
+        self._filename = filename
+        self._cropped_image = []
+        self._cropped_image_name = "cropped_" + filename
+
+    def crop_image(self, x1, y1, x2, y2, target_aspect_ratio):
+        # compute current aspect ratio
+        # from the top corner, resize the shorter dimension to fit the target aspect ratio
+        # if the resize puts us over the target image size...
+        # try from the bottom point
+        # if that fails... ?
+        self._set_crop_target()
+        if target_aspect_ratio != 0:
+            current_aspect_ratio = self._crop_height / float(self._crop_width)
+            if current_aspect_ratio != target_aspect_ratio:
+                if self._crop_width > self._crop_height:
+                    target_crop_height = self._crop_width * target_aspect_ratio
+                    self._lower_point['y'] = self._upper_point['y'] + int(target_crop_height)
+                elif self._crop_width < self._crop_height: 
+                    target_crop_width = self._crop_height * target_aspect_ratio
+                    self._lower_point['x'] = self._upper_point['x'] + int(target_crop_width)
+
+        self._cropped_image = self.source_image[self._upper_point['y']:self._lower_point['y'], 
+                                                self._upper_point['x']:self._lower_point['x']]
+
+    def write_cropped_image(self, destination_directory):
+        cv2.imwrite(destination_directory + self._cropped_filename, self._cropped_image) 
+
+    def _set_crop_target(self, x1, y1, x2, y2):
+        if y1 > y2:
+            self._upper_point['x'] = x1
+            self._upper_point['y'] = y1 
+            self._lower_point['x'] = x2
+            self._lower_point['y'] = y2
+        else: 
+            self._upper_point['x'] = x2
+            self._upper_point['y'] = y2 
+            self._lower_point['x'] = x1
+            self._lower_point['y'] = y1
+        self._crop_width =  abs(x1 - x2)
+        self._crop_height = abs(y1 - y2) 
+
+    def show_source_image(self):
+        cv2.imshow(PICK_CV2_WINDOW_HANDLE, self._source_image)
+
+    def show_cropped_image(self):
+        cv2.imshow(PICK_CV2_WINDOW_HANDLE, self._cropped_image)
+
 
 def get_filename(index):
-    return image_path + image_prefix + str(index) + image_extension
+    return args.image_path + args.image_prefix + str(index) + args.image_extension
 
 def load_checkpoint():
     try:
@@ -20,30 +105,29 @@ def load_checkpoint():
         return 1
 
 def on_mouse(event, x, y, flags, params):
-    draw = False
     images_metadata = params[0]
     filename = params[1]
     img = params[2]
+    if not images_metadata[filename]['clicked']:
+        cv2.imshow(PICK_CV2_WINDOW_HANDLE, img)
     if event == cv.CV_EVENT_LBUTTONDOWN:
-        print 'Start Mouse Position: '+str(x)+', '+str(y)
+        print 'Start Mouse Position: ' + str(x) + ', ' + str(y)
         p1 = [x, y]
+        images_metadata[filename]['p2'] = []
+        images_metadata[filename]['clicked'] = True 
         if p1 != images_metadata[filename]['p1']: 
             images_metadata[filename]['p1'] = p1
-            draw = True
+    elif event == cv.CV_EVENT_MOUSEMOVE and images_metadata[filename]['clicked']:
+        rect_img = img.copy()
+        cv2.rectangle(rect_img, (images_metadata[filename]['p1'][0], images_metadata[filename]['p1'][1]),
+                     (x, y), (155, 55, 200), 2)
+        cv2.imshow(PICK_CV2_WINDOW_HANDLE, rect_img)
     elif event == cv.CV_EVENT_LBUTTONUP:
-        print 'End Mouse Position: '+str(x)+', '+str(y)
+        print 'End Mouse Position: ' + str(x) + ', ' + str(y)
         p2 = [x, y]
         if p2 != images_metadata[filename]['p2']: 
             images_metadata[filename]['p2'] = p2
-            draw = True
-    if draw:
-        draw_box(img, images_metadata, filename)
-
-def draw_box(img, images_metadata, filename):
-    if len(images_metadata[filename]['p1']) == 2 and len(images_metadata[filename]['p2']) == 2:
-        cv2.rectangle(img, (images_metadata[filename]['p1'][0], images_metadata[filename]['p1'][1]), 
-                          (images_metadata[filename]['p2'][0], images_metadata[filename]['p2'][1]), (155, 55, 200), 2)
-        cv2.imshow("Image", img)
+            images_metadata[filename]['clicked'] = False 
 
 
 def mark_images(image_count, images_metadata):
@@ -54,11 +138,12 @@ def mark_images(image_count, images_metadata):
         images_metadata[filename] = {}
         images_metadata[filename]['p1'] = []
         images_metadata[filename]['p2'] = []
+        images_metadata[filename]['clicked'] = False
 
-        cv2.namedWindow("Image")
-        cv.SetMouseCallback("Image", on_mouse, [images_metadata, filename, img])
+        cv2.namedWindow(PICK_CV2_WINDOW_HANDLE)
+        cv.SetMouseCallback(PICK_CV2_WINDOW_HANDLE, on_mouse, [images_metadata, filename, img])
         cv2.putText(img, str(count), (1,20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255))
-        cv2.imshow("Image", img)
+        cv2.imshow(PICK_CV2_WINDOW_HANDLE, img)
         key = cv2.waitKey(0)
         if len(images_metadata[filename]['p1']) == 2 and len(images_metadata[filename]['p2']) == 2:
             write_metadata(count, filename, images_metadata)
