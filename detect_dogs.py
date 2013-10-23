bd0013ca0819cc25e99250b66a21b9b485625dfe
Adam Fletcher <adamf@csh.rit.edu>
2013-10-23 14:55:38 -0400

    Refactor of pick_animal in preperation for change in logic

--- detect_dogs.py
+++ detect_dogs.py
@@ -1,22 +1,34 @@
 import cv2, cv
 
 
-
+good_eyes = open('dog_eyes.dat', 'w')
 DOWNSCALE = 1
-#classifier = cv2.CascadeClassifier("trained/cascade.xml")
+classifier = cv2.CascadeClassifier("trained/cascade.xml")
 #classifier = cv2.CascadeClassifier("/opt/local/share/OpenCV/haarcascades/haarcascade_eye.xml")
 #classifier = cv2.CascadeClassifier("/opt/local/share/OpenCV/haarcascades/haarcascade_mcs_eyepair_big.xml")
-classifier = cv2.CascadeClassifier("/opt/local/share/OpenCV/haarcascades/haarcascade_mcs_eyepair_small.xml")
-for count in range(1,23):
-    filename = '/Users/adamfletcher/Downloads/train/dog.' + str(count) + '.jpg'
+#classifier = cv2.CascadeClassifier("/opt/local/share/OpenCV/haarcascades/haarcascade_mcs_eyepair_small.xml")
+for count in range(1,1000):
+    filename = '/Users/adamfletcher/Downloads/train/bw/cat.' + str(count) + '.jpg'
     img = cv2.imread(filename)
     dogs = classifier.detectMultiScale(img)
 
 
-    for f in dogs:
-        print f
-        x, y, w, h = [ v*DOWNSCALE for v in f ]
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255))
-    cv2.imshow("dog", img)
-    key = cv2.waitKey(0)
+    if len(dogs) > 1:
+        print dogs[0]
+        i = 0
+        for d in dogs:
+            x, y, w, h  = [ v*DOWNSCALE for v in d ]
+            cv2.putText(img, str(i), (x,y), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255))
+            cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255))
+            i = i + 1
+        cv2.imshow("dog", img)
+        key = cv2.waitKey(0)
+        if key == 107:
+            # we have a good eye match
+            good_eyes.write(filename + '\n')
+            print filename
+        print key
+        if key == 27:
+            break
 
+good_eyes.close()
--- pick_animal.py
+++ pick_animal.py
@@ -1,66 +1,78 @@
 import cv2, cv
 
-starting_image = 1
-try:
-   checkpoint = open('dogs.checkpoint', 'r')
-   starting_image = int(checkpoint.readline())
-   print starting_image 
-   checkpoint.close()
-except:
-    starting_image = 1
 
+image_path = "/Users/adamfletcher/Downloads/train/bw/"
+image_extension = ".jpg"
+image_prefix = "dog."
+crop_target_directory = "cropped_images/"
 
-boxes = {}
+def get_filename(index):
+    return image_path + image_prefix + str(index) + image_extension
+
+def load_checkpoint():
+    try:
+        checkpoint = open('dogs.checkpoint', 'r')
+        starting_image = int(checkpoint.readline())
+        print "Starting image: " + starting_image 
+        checkpoint.close()
+        return starting_image
+    except:
+        return 1
+
 def on_mouse(event, x, y, flags, params):
     draw = False
+    images_metadata = params[0]
+    filename = params[1]
+    img = params[2]
     if event == cv.CV_EVENT_LBUTTONDOWN:
         print 'Start Mouse Position: '+str(x)+', '+str(y)
         p1 = [x, y]
-        if p1 != boxes[filename]['p1']: 
-            boxes[filename]['p1'] = p1
+        if p1 != images_metadata[filename]['p1']: 
+            images_metadata[filename]['p1'] = p1
             draw = True
     elif event == cv.CV_EVENT_LBUTTONUP:
         print 'End Mouse Position: '+str(x)+', '+str(y)
         p2 = [x, y]
-        if p2 != boxes[filename]['p2']: 
-            boxes[filename]['p2'] = p2
+        if p2 != images_metadata[filename]['p2']: 
+            images_metadata[filename]['p2'] = p2
             draw = True
     if draw:
-        drawBox()
+        draw_box(img, images_metadata, filename)
 
-def drawBox():
-    if len(boxes[filename]['p1']) == 2 and len(boxes[filename]['p2']) == 2:
-    #    print "drawing"
-        cv2.rectangle(img, (boxes[filename]['p1'][0], boxes[filename]['p1'][1]), 
-                          (boxes[filename]['p2'][0], boxes[filename]['p2'][1]), (155, 55, 200), 2)
-        cv2.imshow('real image', img)
-    #    print filename, boxes[filename]['p1'][0], boxes[filename]['p1'][1], boxes[filename]['p2'][0], boxes[filename]['p2'][1]
-
-count = starting_image
-filename = ''
-img = cv2.imread(filename)
-while(count < 100):
-    filename = '/Users/adamfletcher/Downloads/train/dog.' + str(count) + '.jpg'
-    img = cv2.imread(filename)
-    boxes[filename] = {}
-    boxes[filename]['p1'] = []
-    boxes[filename]['p2'] = []
-
-    cv2.namedWindow('real image')
-    cv.SetMouseCallback('real image', on_mouse, 0)
-    cv2.imshow('real image', img)
-    if cv2.waitKey(0) == 27:
-        cv2.destroyAllWindows()
-        break
-    count += 1
+def draw_box(img, images_metadata, filename):
+    if len(images_metadata[filename]['p1']) == 2 and len(images_metadata[filename]['p2']) == 2:
+        cv2.rectangle(img, (images_metadata[filename]['p1'][0], images_metadata[filename]['p1'][1]), 
+                          (images_metadata[filename]['p2'][0], images_metadata[filename]['p2'][1]), (155, 55, 200), 2)
+        cv2.imshow("Image", img)
 
 
-datafile = open('dogs.dat', 'a+')
-for filename in boxes.keys():
-    if len(boxes[filename]['p1']) == 2 and len(boxes[filename]['p2']) == 2:
+def mark_images(image_count, images_metadata):
+    count = image_count
+    while(count < 10000):
+        filename = get_filename(count) 
+        img = cv2.imread(filename)
+        images_metadata[filename] = {}
+        images_metadata[filename]['p1'] = []
+        images_metadata[filename]['p2'] = []
+
+        cv2.namedWindow("Image")
+        cv.SetMouseCallback("Image", on_mouse, [images_metadata, filename, img])
+        cv2.putText(img, str(count), (1,20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255))
+        cv2.imshow("Image", img)
+        key = cv2.waitKey(0)
+        if len(images_metadata[filename]['p1']) == 2 and len(images_metadata[filename]['p2']) == 2:
+            write_metadata(count, filename, images_metadata)
+        if key == 27:
+            cv2.destroyAllWindows()
+            return count
+        count += 1
+
+def write_metadata(image_index, filename, images_metadata):
+    if len(images_metadata[filename]['p1']) == 2 and len(images_metadata[filename]['p2']) == 2:
+        datafile = open('dogs.dat', 'a+')
         # the opencv classifier wants x, y, width, height with x,y being the upper left corner
-        p1 = boxes[filename]['p1']
-        p2 = boxes[filename]['p2']
+        p1 = images_metadata[filename]['p1']
+        p2 = images_metadata[filename]['p2']
         width = p2[0] - p1[0]
         height = p2[1] - p1[1]
         x = p1[0]
@@ -70,14 +82,18 @@ for filename in boxes.keys():
             y = p2[1]
         width = abs(width)
         height = abs(height)
-        starting_image = starting_image + 1
         print filename, x, y, width, height 
         datafile.write("%s 1 %i %i %i %i\n" % (filename, x, y, width, height))
+        checkpoint(image_index)
+        datafile.close()
 
-datafile.close()
+def checkpoint(image_index):
+    checkpoint = open('dogs.checkpoint', 'w')
+    checkpoint.write("%i" % image_index)
+    checkpoint.close()
 
-checkpoint = open('dogs.checkpoint', 'w')
-checkpoint.write("%i" % starting_image)
-checkpoint.close()
 
+starting_image = load_checkpoint()
+images_metadata = {}
+ending_image = mark_images(starting_image, images_metadata)
 
